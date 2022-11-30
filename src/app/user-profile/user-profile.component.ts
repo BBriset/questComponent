f8cd135ebe9dfa98ef4887357f1395cf1686a65d
Bertrand Briset <82585037+BBriset@users.noreply.github.com>
2022-11-30 09:43:48 +0100

    Create user profil card

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css'],
+})
+export class UserProfileComponent implements OnInit {
+  name: string = 'Doe';
+
+  firstName: string = 'John';
+
+  age: number = 25;
+
+  quote: string = '';
+
+  photo: string = 'https://randomuser.me/api/portraits/lego/2.jpg';
+
+  hideAge: boolean = true;
+
+  ngOnInit() {}
+}
